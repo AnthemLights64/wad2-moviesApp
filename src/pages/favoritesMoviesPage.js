bf17ad9f0603c4e955b568d57a0c5f213c9ddfaa
Wenlong Lu <yummybroccoli@outlook.com>
2021-01-17 16:57:45 +0000

    previous login commented out

--- src/pages/favoritesMoviesPage.js
+++ src/pages/favoritesMoviesPage.js
@@ -2,16 +2,16 @@ import React, {useContext} from "react";
 import MovieListPageTemplate from "../components/templateMovieListPage";
 import AddReviewButton from '../components/buttons/addReview'
 import {MoviesContext} from '../contexts/moviesContext'
-import {Link} from "react-router-dom";
+//import {Link} from "react-router-dom";
 
 const FavoriteMoviesPage = props => {
   const context = useContext(MoviesContext);
   const favorites = context.movies.filter( m => m.favorite );
-  const user = context.user;
+  //const user = context.user;
 
-  if(!user){
-    return <div style={{fontSize:35, textAlign:"center"}}><p>Please <span className="signIn"><Link to={'/signIn'}>Sign In</Link></span> first!</p></div>
-  }
+  // if(!user){
+  //   return <div style={{fontSize:35, textAlign:"center"}}><p>Please <span className="signIn"><Link to={'/signIn'}>Sign In</Link></span> first!</p></div>
+  // }
 
   return (
     <MovieListPageTemplate
