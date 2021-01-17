6324b541532a6e3cde107429edc479bbed403a88
Wenlong Lu <yummybroccoli@outlook.com>
2021-01-17 16:47:23 +0000

    router protected and login signup implemented

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import SiteHeader from './components/siteHeader'
 import MoviesContextProvider from "./contexts/moviesContext";
 import GenresContextProvider from "./contexts/genresContext";
 import PeopleContextProvider from "./contexts/peopleContext";
+
+
+import PrivateRoute from "./components/privateRoute";
+import AuthHeader from "./components/authHeader";
+import AuthProvider from "./contexts/authContext";
+
 //import HomePage from "./pages/homePage";
 //import MoviePage from './pages/movieDetailsPage';
 //import FavoriteMoviesPage from './pages/favoritesMoviesPage'     ;  // NEW
@@ -36,39 +42,46 @@ const CastDetailsPage = lazy(() => import("./pages/castDetailsPage"));
 const SignInPage = lazy(() => import("./pages/signInPage"));
 const PeoplePage = lazy(() => import("./pages/peoplePage"));
 const PersonDetailsPage = lazy(() => import("./pages/personDetailsPage"));
+const LoginPage = lazy(() => import("./pages/loginPage"));
+const SignUpPage = lazy(() => import("./pages/signUpPage"));
 
 const App = () => {
   return (
   <BrowserRouter>
   <div className="jumbotron">
-    <SiteHeader /> 
-    <div className="container-fluid">
-      <MoviesContextProvider>     {/* NEW  */}
-        <GenresContextProvider>    {/* NEW */}
-          <PeopleContextProvider>
-            <Suspense fallback={<h1>Loading page....</h1>}>
-                <Switch> 
-                  <Route path="/person/popular" component={PeoplePage} />
-                  <Route path="/person/:id" component={PersonDetailsPage} />
-                  <Route path="/signIn" component={SignInPage} />
-                  <Route path="/credit/:id" component={CastDetailsPage} />
-                  <Route exact path="/movies/:id/movieCredits" component={MovieCreditsPage} />
-                  <Route path="/:id/similar" component={SimilarMoviesPage} />
-                  <Route exact path="/movies/watchList" component={WatchListPage} />
-                  <Route exact path="/movies/top_rated" component={TopRatedMoviesPage} />
-                  <Route exact path="/reviews/form" component={AddMovieReviewPage} />
-                  <Route path="/reviews/:id" component={MovieReviewPage} />
-                  <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
-                  <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-                  <Route path="/movies/:id" component={MoviePage} />
-                  <Route path="/" component={HomePage} />
-                  <Redirect from="*" to="/" />
-                </Switch>
-            </Suspense>
-          </PeopleContextProvider>
-        </GenresContextProvider>    {/* NEW */}
-      </MoviesContextProvider>     {/* NEW */}
-    </div>
+    <AuthProvider>
+      <AuthHeader />
+      <SiteHeader /> 
+      <div className="container-fluid">
+        <MoviesContextProvider>     {/* NEW  */}
+          <GenresContextProvider>    {/* NEW */}
+            <PeopleContextProvider>
+              <Suspense fallback={<h1>Loading page....</h1>}>
+                  <Switch>
+                    <Route path="/login" component={LoginPage} />
+                    <Route path="/signup" component={SignUpPage} /> 
+                    <Route path="/person/popular" component={PeoplePage} />
+                    <Route path="/person/:id" component={PersonDetailsPage} />
+                    <Route path="/signIn" component={SignInPage} />
+                    <Route path="/credit/:id" component={CastDetailsPage} />
+                    <Route exact path="/movies/:id/movieCredits" component={MovieCreditsPage} />
+                    <Route path="/:id/similar" component={SimilarMoviesPage} />
+                    <Route exact path="/movies/watchList" component={WatchListPage} />
+                    <Route exact path="/movies/top_rated" component={TopRatedMoviesPage} />
+                    <Route exact path="/reviews/form" component={AddMovieReviewPage} />
+                    <Route path="/reviews/:id" component={MovieReviewPage} />
+                    <PrivateRoute exact path="/movies/upcoming" component={UpcomingMoviesPage} />
+                    <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+                    <Route path="/movies/:id" component={MoviePage} />
+                    <PrivateRoute path="/" component={HomePage} />
+                    <Redirect from="*" to="/" />
+                  </Switch>
+              </Suspense>
+            </PeopleContextProvider>
+          </GenresContextProvider>    {/* NEW */}
+        </MoviesContextProvider>     {/* NEW */}
+      </div>
+    </AuthProvider>
   </div>
 </BrowserRouter>
   );
